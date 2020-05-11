f8e6ad1edb2ffc23d47df7468ec7fcf765f30f68
Denis Dergunov <dan.dergunow@gmail.com>
2020-05-11 23:47:19 +0300

    telnet_server: utils functions description added

--- telnet_server/filter_utils.hpp
+++ telnet_server/filter_utils.hpp
@@ -23,8 +23,10 @@ namespace code_bytes{
     constexpr char IAC = -1; //0xFF
 }
 
+//filter all carriage return (CR) symbols
 void filter_r(std::string& buf, std::size_t& str_size);
 
+//replace line feed (LF) with CR LF
 void add_r(std::string& buf, std::size_t& str_size);
 
 #endif // FILTER_UTILS_H
