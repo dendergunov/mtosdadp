2f2c9ce250995b28b08e21df5ceca139c31f3cdd
Denis Dergunov <dan.dergunow@gmail.com>
2020-06-17 12:07:56 +0300

    lfsr_intrinsic: moved bitset to bitset_lfsr.hpp for clarity

--- lfsr_intrinsic/sisd_lfsr.hpp
+++ lfsr_intrinsic/sisd_lfsr.hpp
@@ -57,41 +57,7 @@ private:
     std::array<std::uint64_t, bit_width/64> state_;
 };
 
-template<std::size_t bit_width>
-class bitset_lfsr
-{
-public:
-    bitset_lfsr(std::array<std::size_t, 4> tap_indicies)
-        : bit_width_(bit_width),
-        width_(bit_width / 64)
-    {
-        static_assert(bit_width>0 && !(bit_width%64), "Number of bits has to be a multiple of 64!");
-        for(auto x: tap_indicies)
-            polynom_.set(x);
-        for(int i = 0; i < bit_width_; ++i)
-            state_.set(i, uniform_random(0, 1));
-    }
 
-    void print_state(){
-        std::cout << "State:\n";
-        std::cout << state_ << std::endl;
-    }
-
-    bool clock(){
-        bool to_tap = state_[0];
-        if(to_tap)
-            state_ ^= polynom_;
-        state_ = state_ >> 1;
-        state_[bit_width_-1] = to_tap;
-        return to_tap;
-    }
-
-private:
-    std::size_t bit_width_;
-    std::size_t width_;
-    std::bitset<bit_width> polynom_;
-    std::bitset<bit_width> state_;
-};
 
 
 #endif // SISD_LFSR_HPP
