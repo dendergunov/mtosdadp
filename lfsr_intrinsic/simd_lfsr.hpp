70d2cafb6aba34af0772e3ea5e27c1c1b46df0d7
Denis Dergunov <dan.dergunow@gmail.com>
2020-06-17 13:19:18 +0300

    lfsr_intrinsic: sse version changes
    now it works with full-length polynomials
    two constructors using std::array and std::vector respectively
    initialization and indexing replaced with corresponding intrinsics

--- lfsr_intrinsic/simd_lfsr.hpp
+++ lfsr_intrinsic/simd_lfsr.hpp
@@ -12,15 +12,17 @@ template <std::size_t bit_width>
 class simd_lfsr_128
 {
 public:
-    simd_lfsr_128(std::uint64_t polynom_head)
+    simd_lfsr_128(const std::array<std::uint64_t, bit_width/64>& poly)
         : bit_width_(bit_width),
-            width_(bit_width_/128),
-        polynom_head_(polynom_head)
+            width_(bit_width_/128)
     {
         static_assert(bit_width>0 && !(bit_width%128), "Number of bits has to be a multiple of 128!");
         for(auto& x: state_){
-            x[0] = uniform_random(0, std::numeric_limits<std::uint64_t>::max());
-            x[1] = uniform_random(0, std::numeric_limits<std::uint64_t>::max());
+            x = _mm_set_epi64x(uniform_random(0, std::numeric_limits<std::uint64_t>::max()),
+                              uniform_random(0, std::numeric_limits<std::uint64_t>::max()));
+        }
+        for(int i = 0; i < width_; ++i){
+            polynom_[i] = _mm_set_epi64x(poly[2*i+1], poly[2*i]);
         }
     }
 
@@ -28,25 +30,28 @@ public:
     {
         std::cout << "State\n";
         for (auto i = state_.rbegin(); i != state_.rend(); ++i){
-            std::cout << std::bitset<64>((*i)[1]) << '|' << std::bitset<64>((*i)[0]) << std::endl;
+            std::cout << std::bitset<64>(_mm_extract_epi64(*i, 1))
+            << '|' << std::bitset<64>(_mm_extract_epi64(*i, 0))
+            << std::endl;
         }
     }
 
     bool clock()
     {
-        bool output_bit = state_[0][0]&0x1ull;
+        bool output_bit = _mm_extract_epi8(state_[0], 0) & 0x1;
         bool ob = output_bit;
         bool carrier_bit = output_bit;
-        if(output_bit){
-            state_[width_-1][1] ^= polynom_head_;
-        }
+
         __m128i least_bits;
-        constexpr __m128i mask = {0x1, 0x1};
+        __m128i mask = _mm_set_epi64x(0x1, 0x1);
         for(int i = width_-1; i >= 0; --i){
+            if(ob){
+                state_[i] = _mm_xor_si128(state_[i], polynom_[i]);
+            }
             least_bits = _mm_and_si128(state_[i], mask);
-            output_bit = least_bits[0];
+            output_bit = _mm_extract_epi8(least_bits, 0);
             least_bits = _mm_srli_si128(least_bits, 8);
-            least_bits[1] = carrier_bit;
+            least_bits = _mm_insert_epi64(least_bits, carrier_bit, 1);
             least_bits = _mm_slli_epi64(least_bits, 63);
             state_[i] = _mm_srli_epi64(state_[i], 1);
             state_[i] = _mm_or_si128(state_[i], least_bits);
@@ -60,7 +65,7 @@ private:
     std::size_t bit_width_;
     std::size_t width_;
     std::array<__m128i, bit_width/128> state_;
-    std::uint64_t polynom_head_;
+    std::array<__m128i, bit_width/128> polynom_;
 };
 #endif
 
