275ed57e5831a0f7d3bc9446606a84b4b321e5ba
Denis Dergunov <dan.dergunow@gmail.com>
2020-06-17 14:16:29 +0300

    lfsr_intrinsic: avx2 version changes
    now supports full-length polynoms
    two constructor using std::arary and std::vector
    indexing and initialization replaced with intrinsics

--- lfsr_intrinsic/simd_lfsr.hpp
+++ lfsr_intrinsic/simd_lfsr.hpp
@@ -54,6 +54,16 @@ public:
         }
     }
 
+    void print_polynom()
+    {
+        std::cout << "State\n";
+        for (auto i = polynom_.rbegin(); i != polynom_.rend(); ++i){
+            std::cout << std::bitset<64>(_mm_extract_epi64(*i, 1))
+                      << '|' << std::bitset<64>(_mm_extract_epi64(*i, 0))
+                      << std::endl;
+        }
+    }
+
     bool clock()
     {
         bool output_bit = _mm_extract_epi8(state_[0], 0) & 0x1;
@@ -93,17 +103,39 @@ template <std::size_t bit_width>
 class simd_lfsr_256
 {
 public:
-    simd_lfsr_256(std::uint64_t polynom_head)
+    simd_lfsr_256(const std::array<std::uint64_t, bit_width/64>& poly)
         : bit_width_(bit_width),
-        width_(bit_width_/256),
-        polynom_head_(polynom_head)
+        width_(bit_width_/256)
     {
         static_assert(bit_width>0 && !(bit_width%256), "Number of bits has to be a multiple of 256!");
         for(auto& x: state_){
-            x[0] = uniform_random(0, std::numeric_limits<std::uint64_t>::max());
-            x[1] = uniform_random(0, std::numeric_limits<std::uint64_t>::max());
-            x[2] = uniform_random(0, std::numeric_limits<std::uint64_t>::max());
-            x[3] = uniform_random(0, std::numeric_limits<std::uint64_t>::max());
+            x = _mm256_set_epi64x(uniform_random(0, std::numeric_limits<std::uint64_t>::max()),
+                                   uniform_random(0, std::numeric_limits<std::uint64_t>::max()),
+                                   uniform_random(0, std::numeric_limits<std::uint64_t>::max()),
+                                   uniform_random(0, std::numeric_limits<std::uint64_t>::max()));
+        }
+        for(int i = 0; i < width_; ++i){
+            polynom_[i] = _mm256_set_epi64x(poly[4*i+3], poly[4*i+2],
+                                         poly[4*i+1], poly[4*i]);
+        }
+    }
+
+    simd_lfsr_256(const std::vector<std::uint64_t>& poly)
+        : bit_width_(bit_width),
+        width_(bit_width_/256)
+    {
+        static_assert(bit_width>0 && !(bit_width%256), "Number of bits has to be a multiple of 256!");
+        if(poly.size()<width_*4)
+            throw std::runtime_error("Vector with coefficients is too small!");
+        for(auto& x: state_){
+            x = _mm256_set_epi64x(uniform_random(0, std::numeric_limits<std::uint64_t>::max()),
+                                  uniform_random(0, std::numeric_limits<std::uint64_t>::max()),
+                                  uniform_random(0, std::numeric_limits<std::uint64_t>::max()),
+                                  uniform_random(0, std::numeric_limits<std::uint64_t>::max()));
+        }
+        for(int i = 0; i < width_; ++i){
+            polynom_[i] = _mm256_set_epi64x(poly[4*i+3], poly[4*i+2],
+                                         poly[4*i+1], poly[4*i]);
         }
     }
 
@@ -111,29 +143,41 @@ public:
     {
         std::cout << "State\n";
         for (auto i = state_.rbegin(); i != state_.rend(); ++i){
-            std::cout << std::bitset<64>((*i)[3]) << '|' << std::bitset<64>((*i)[2]) << '|'
-                      << std::bitset<64>((*i)[1]) << '|' << std::bitset<64>((*i)[0]) << std::endl;
+            std::cout << std::bitset<64>(_mm256_extract_epi64(*i, 3))
+                      << '|' << std::bitset<64>(_mm256_extract_epi64(*i, 2))
+                      << '|' << std::bitset<64>(_mm256_extract_epi64(*i, 1))
+                      << '|' << std::bitset<64>(_mm256_extract_epi64(*i, 0))
+                      << std::endl;
+        }
+    }
+
+    void print_polynom()
+    {
+        std::cout << "Polynom\n";
+        for (auto i = polynom_.rbegin(); i != polynom_.rend(); ++i){
+            std::cout << std::bitset<64>(_mm256_extract_epi64(*i, 3))
+                      << '|' << std::bitset<64>(_mm256_extract_epi64(*i, 2))
+                      << '|' << std::bitset<64>(_mm256_extract_epi64(*i, 1))
+                      << '|' << std::bitset<64>(_mm256_extract_epi64(*i, 0))
+                      << std::endl;
         }
     }
 
     bool clock()
     {
-        bool output_bit = state_[0][0]&0x1ull;
+        bool output_bit = _mm256_extract_epi8(state_[0], 0) & 0x1;
         bool ob = output_bit;
         bool carrier_bit = output_bit;
-        if(output_bit){
-            state_[width_-1][3] ^= polynom_head_;
-        }
         __m256i least_bits;
-        constexpr __m256i mask = {0x1, 0x1, 0x1, 0x1};
-        bool inner_carrier_bit;
+        __m256i mask = _mm256_set_epi64x(0x1, 0x1, 0x1, 0x1);
         for(int i = width_-1; i >= 0; --i){
+            if(ob){
+                state_[i] = _mm256_xor_si256(state_[i], polynom_[i]);
+            }
             least_bits = _mm256_and_si256(state_[i], mask);
-            output_bit = least_bits[0];
-            inner_carrier_bit = least_bits[2];
-            least_bits = _mm256_srli_si256(least_bits, 8);
-            least_bits[1] = inner_carrier_bit;
-            least_bits[3] = carrier_bit;
+            output_bit = _mm256_extract_epi8(least_bits, 0);
+            least_bits = _mm256_permute4x64_epi64(least_bits, 0b00111001);
+            least_bits = _mm256_insert_epi64(least_bits, carrier_bit, 3);
             least_bits = _mm256_slli_epi64(least_bits, 63);
             state_[i] = _mm256_srli_epi64(state_[i], 1);
             state_[i] = _mm256_or_si256(state_[i], least_bits);
@@ -147,7 +191,7 @@ private:
     std::size_t bit_width_;
     std::size_t width_;
     std::array<__m256i, bit_width/256> state_;
-    std::uint64_t polynom_head_;
+    std::array<__m256i, bit_width/256> polynom_;
 };
 #endif
 
