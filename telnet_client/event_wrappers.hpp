e69fb0252a88a003e5e11bb16ea077a96ad8b313
Denis Dergunov <dan.dergunow@gmail.com>
2020-06-24 17:28:16 +0300

    telnet_client: add event wrapper

--- telnet_client/event_wrappers.hpp
+++ telnet_client/event_wrappers.hpp
@@ -31,6 +31,8 @@ struct event_base_w
         event_base_free(base);
         base = other.base;
         other.base = nullptr;
+
+        return *this;
     }
 };
 
@@ -40,7 +42,7 @@ struct bufferevent_w
 
     bufferevent_w(event_base_w& ebw)
     {
-         bufferevent_socket_new(ebw.base, -1, BEV_OPT_CLOSE_ON_FREE);
+         bev = bufferevent_socket_new(ebw.base, -1, BEV_OPT_CLOSE_ON_FREE);
          if(!bev)
              throw std::runtime_error("Cannot create bufferevent\n");
     }
@@ -52,6 +54,8 @@ struct bufferevent_w
         bufferevent_free(bev);
         bev = other.bev;
         other.bev = nullptr;
+
+        return *this;
     }
     bufferevent_w(const bufferevent_w& other) = delete;
     bufferevent_w& operator=(const bufferevent_w& other) = delete;
@@ -59,6 +63,32 @@ struct bufferevent_w
     ~bufferevent_w()
         { bufferevent_free(bev); }
 };
+
+struct event_w
+{
+    struct event* ev;
+    event_w() : ev(nullptr)
+    {}
+
+    event_w(event_w&& other)
+        : ev(other.ev)
+    { other.ev = nullptr;}
+
+    event_w& operator=(event_w&& other)
+    {
+        event_free(ev);
+        ev = other.ev;
+        other.ev = nullptr;
+
+        return *this;
+    }
+    event_w(const event_w& other) = delete;
+    event_w& operator=(const event_w& other) = delete;
+
+    ~event_w()
+        { event_free(ev); }
+};
+
 
 }
 
